5383fabc75f775430223bb1526c170c69e9cb061
Mythpatgaming <97614695+Mythpatgaming@users.noreply.github.com>
2022-03-07 18:52:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function preload(){"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUtuE_mjwE5aDXNPs87WFleIkjOkaLgA8Q9Q&usqp=CAU"}
+function setup(){
+    canvas=createCanvas(300, 300);
+    canvas.center();
+}
+function draw(){}
+function take_snapshot(){
+   save("mustache.png");    
+}
